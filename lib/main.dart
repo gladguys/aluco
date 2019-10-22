8f8e853a17d87540217ffa23fd045e7854d3b264
Rodrigo Bastos <rodrigobastosv@gmail.com>
2019-10-22 09:20:52 -0300

    Implementing conectivity

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:connectivity/connectivity.dart';
 import 'package:flutter/material.dart';
 
 import 'package:aluco/screen/signin/signin_screen.dart';
@@ -5,7 +6,20 @@ import 'theme/main_theme.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final _subscription =  Connectivity().onConnectivityChanged.listen((result) {
+    if (result == ConnectivityResult.none) {
+      print('sem net');
+    } else {
+      print('com net');
+    }
+  });
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,5 +27,11 @@ class MyApp extends StatelessWidget {
       theme: theme,
       home: SigninScreen(),
     );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _subscription.cancel();
   }
 }
