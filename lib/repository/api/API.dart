89b1083b552abfcc6b846f5d0db0a80538f8e545
Rodrigo Bastos <rodrigobastosv@gmail.com>
2019-10-27 22:29:24 -0300

    Adjusting endpoint

--- lib/repository/api/API.dart
+++ lib/repository/api/API.dart
@@ -1,5 +1,5 @@
 const API_URL = 'http://aluco-dev.herokuapp.com/api/';
 const AUTH = 'auth';
-const CLASS = 'class';
+const CLASS = 'classes';
 const STUDENT = 'students';
 const TEACHER = 'teachers';
