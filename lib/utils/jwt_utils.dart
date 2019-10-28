5a0d3ca08dadcf6fd0076badb645eac3256ec318
Rodrigo Bastos <rodrigobastosv@gmail.com>
2019-10-27 22:31:24 -0300

    Storing username on storage

--- lib/utils/jwt_utils.dart
+++ lib/utils/jwt_utils.dart
@@ -5,10 +5,15 @@ class JWTUtils {
   static Future<void> storeInfo(Map<String, dynamic> loggedUserData) async {
     await preferences.setInt('userId', loggedUserData['userId']);
     await preferences.setString('token', loggedUserData['token']);
+    await preferences.setString('username', loggedUserData['username']);
   }
 
   static String getToken() {
     return preferences.getString('token');
+  }
+
+  static String getUsername() {
+    return preferences.getString('username');
   }
 
   static int getTeacherId() {
