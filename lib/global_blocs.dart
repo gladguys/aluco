92766b6865ecd620df54ccb94b3da12d3242918d
Rodrigo Bastos <rodrigobastosv@gmail.com>
2019-11-01 21:50:36 -0300

    Creating and adding the bloc

--- lib/global_blocs.dart
+++ lib/global_blocs.dart
@@ -1,11 +1,13 @@
 import 'package:bloc_pattern/bloc_pattern.dart';
 
 import 'screen/classes/class_home/class_home_bloc.dart';
+import 'screen/classes/class_home/class_students/class_students_bloc.dart';
 import 'screen/classes/list_classes/list_classes_bloc.dart';
 import 'screen/student/student_bloc.dart';
 
 final globalBlocs = [
   Bloc((i) => ClassHomeBloc()),
+  Bloc((i) => ClassStudentsBloc()),
   Bloc((i) => ListClassesBloc()),
   Bloc((i) => StudentBloc()),
 ];
---
+++ lib/screen/classes/class_home/class_students/class_students_bloc.dart
@@ -0,0 +1,88 @@
+import 'package:aluco/model/student.dart';
+import 'package:aluco/model/student_marked.dart';
+import 'package:aluco/repository/api/class_repository.dart';
+import 'package:aluco/repository/api/student_repository.dart';
+import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:rxdart/rxdart.dart';
+
+class ClassStudentsBloc extends BlocBase {
+  final allStudentsStream = BehaviorSubject<List<Student>>.seeded([]);
+  List<Student> get allStudents => allStudentsStream.value;
+
+  final allStudentsMarkedStream = BehaviorSubject<List<StudentMarked>>.seeded([]);
+  List<StudentMarked> get allStudentsMarked => allStudentsMarkedStream.value;
+
+  final studentsToAddStream = BehaviorSubject<List<Student>>.seeded([]);
+  List<Student> get studentsToAdd => studentsToAddStream.value;
+
+  final studentsInClassStream = BehaviorSubject<List<Student>>.seeded([]);
+  List<Student> get studentsInClass => studentsInClassStream.value;
+
+  final studentsNotInClassStream = BehaviorSubject<List<Student>>.seeded([]);
+  List<Student> get studentsNotInClass => studentsNotInClassStream.value;
+
+  int classId;
+  final _classRepository = ClassRepository();
+  final _studentRepository = StudentRepository();
+
+  Future<void> initStudents(int classId) async {
+    this.classId = classId;
+    allStudents.addAll(await _studentRepository.getAll());
+    studentsInClass.addAll(await _classRepository.getStudents(classId));
+    studentsNotInClass.addAll([...allStudents]
+      ..removeWhere((student) => studentsInClass.indexWhere((s) => s.id == student.id) != -1));
+
+    allStudentsStream.add(allStudents);
+    studentsInClassStream.add(studentsInClass);
+    studentsNotInClassStream.add(studentsNotInClass);
+    markAllNotInCLassStudents();
+  }
+
+  void markAllNotInCLassStudents() {
+    final studentsNotInClass = studentsNotInClassStream.value;
+    final markedStudents = studentsNotInClass
+        .map((student) => StudentMarked(student: student))
+        .toList();
+
+    allStudentsMarkedStream.add(markedStudents);
+  }
+
+  void markStudent(Student student, bool marked) {
+    final allStudentsMarked = allStudentsMarkedStream.value;
+    final studentsInClass = studentsInClassStream.value;
+    final index = allStudentsMarked.indexWhere((markedStudent) => markedStudent.student.id == student.id);
+    if (index != -1) {
+      allStudentsMarked[index].marked = marked;
+
+      if (marked) {
+        studentsToAdd.add(student);
+        studentsNotInClass.removeWhere((notInClassStudent) => notInClassStudent.id == student.id);
+      } else {
+        studentsToAdd.removeWhere((studentsInClass) => studentsInClass.id == student.id);
+        studentsNotInClass.add(student);
+      }
+    }
+    studentsInClassStream.add(studentsInClass);
+    studentsNotInClassStream.add(studentsNotInClass);
+    allStudentsMarkedStream.add(allStudentsMarked);
+  }
+
+  Future<void> saveNewStudents() async {
+    try {
+      await _classRepository.saveStudents(classId, studentsToAdd);
+      studentsInClass.addAll(studentsToAdd);
+      studentsInClassStream.add(studentsInClass);
+    } catch (e) {
+      print(e);
+      throw Exception();
+    }
+  }
+
+  void cleanBloc() {
+    allStudentsStream.add([]);
+    allStudentsMarkedStream.add([]);
+    studentsToAddStream.add([]);
+    studentsInClassStream.add([]);
+    studentsNotInClassStream.add([]);
+  }
+}
