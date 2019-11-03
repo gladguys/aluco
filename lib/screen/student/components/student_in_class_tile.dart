8e806edcb87a2515570e47bbc6385dce4e263735
Rodrigo Bastos <rodrigobastosv@gmail.com>
2019-11-03 01:17:09 -0300

    Putting stream builders to optimize the build

--- lib/screen/student/components/student_in_class_tile.dart
+++ lib/screen/student/components/student_in_class_tile.dart
@@ -1,32 +1,38 @@
 import 'package:aluco/model/student_marked.dart';
 import 'package:aluco/screen/classes/class_home/class_students/class_students_bloc.dart';
+import 'package:aluco/widget/al_waiting_indicator.dart';
 import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
 
-class StudentInClassTile extends StatefulWidget {
+class StudentInClassTile extends StatelessWidget {
   const StudentInClassTile(this.markedStudent);
 
   final StudentMarked markedStudent;
 
   @override
-  _StudentInClassTileState createState() => _StudentInClassTileState();
-}
-
-class _StudentInClassTileState extends State<StudentInClassTile> {
-  @override
   Widget build(BuildContext context) {
-    return CheckboxListTile(
-      key: ValueKey(widget.markedStudent.student.id),
-      title: Text(widget.markedStudent.student.name),
-      selected: widget.markedStudent.marked,
-      value: widget.markedStudent.marked,
-      onChanged: (marked) {
-        BlocProvider.getBloc<ClassStudentsBloc>()
-            .markStudent(widget.markedStudent.student, marked);
+    final int index = BlocProvider.getBloc<ClassStudentsBloc>()
+        .allStudentsMarkedListController
+        .indexWhere((studentController) =>
+            studentController.value.student.id == markedStudent.student.id);
 
-        // TODO(rodrigo): se livrar disso depois
-        setState((){});
-      },
-    );
+    return StreamBuilder<StudentMarked>(
+        key: ValueKey(markedStudent.student.id),
+        stream: BlocProvider.getBloc<ClassStudentsBloc>()
+            .allStudentsMarkedListController[index],
+        builder: (_, snapshot) {
+          if (snapshot.hasData) {
+            return CheckboxListTile(
+              title: Text(markedStudent.student.name),
+              selected: markedStudent.marked,
+              value: markedStudent.marked,
+              onChanged: (marked) {
+                BlocProvider.getBloc<ClassStudentsBloc>()
+                    .markStudent(markedStudent.student, marked);
+              },
+            );
+          }
+          return ALWaitingIndicator();
+        });
   }
 }
