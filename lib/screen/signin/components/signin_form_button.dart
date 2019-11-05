e73181aa904b1aab560ca7e0daf42d2c29fe75a6
Rodrigo Bastos <rodrigobastosv@gmail.com>
2019-11-05 00:39:52 -0300

    Correcting feedbacks on signin and signup screens

--- lib/screen/signin/components/signin_form_button.dart
+++ lib/screen/signin/components/signin_form_button.dart
@@ -4,15 +4,35 @@ import 'package:aluco/utils/form_utils.dart';
 import 'package:aluco/widget/al_waiting_indicator.dart';
 import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
+import 'package:gg_flutter_components/gg_snackbar.dart';
 
 import '../signin_bloc.dart';
 import 'signin_form.dart';
 
-class SigninFormButton extends StatelessWidget {
-  SigninFormButton({this.signinForm});
+class SigninFormButton extends StatefulWidget {
+  const SigninFormButton({this.signinForm});
 
-  final SigninBloc _bloc = BlocProvider.getBloc<SigninBloc>();
   final SigninForm signinForm;
+
+  @override
+  _SigninFormButtonState createState() => _SigninFormButtonState();
+}
+
+class _SigninFormButtonState extends State<SigninFormButton> {
+  final SigninBloc _bloc = BlocProvider.getBloc<SigninBloc>();
+
+  @override
+  void initState() {
+    _bloc.signinStateController.listen(
+      (signInState) {
+        if (signInState == SigninState.wrongCredentials) {
+          GGSnackbar.error(
+              message: 'Usuário ou senha inválidos', context: context);
+        }
+      },
+    );
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,25 +49,31 @@ class SigninFormButton extends StatelessWidget {
       child: StreamBuilder(
         stream: _bloc.signinStateController,
         builder: (_, snapshot) {
-          if (snapshot.data == SigninState.idle) {
-            return const Text(
-              'Entrar',
-              style: TextStyle(
-                fontSize: 16.0,
-              ),
-            );
-          } else {
-            return const SizedBox(
-              height: 18,
-              width: 48,
-              child: ALWaitingIndicator(size: 20),
-            );
+          switch (snapshot.data) {
+            case SigninState.idle:
+            case SigninState.failed:
+            case SigninState.succeeded:
+            case SigninState.wrongCredentials:
+              return const Text(
+                'Entrar',
+                style: TextStyle(
+                  fontSize: 16.0,
+                ),
+              );
+            case SigninState.onGoing:
+            default:
+              return const SizedBox(
+                height: 18,
+                width: 48,
+                child: ALWaitingIndicator(size: 20),
+              );
           }
         },
       ),
       onPressed: () async {
-        if (FormUtils.isValid(signinForm.getForm())) {
-          final loggedUserData = await _bloc.tryToSigninUser(signinForm.data);
+        if (FormUtils.isValid(widget.signinForm.getForm())) {
+          final loggedUserData =
+              await _bloc.tryToSigninUser(widget.signinForm.data);
           if (loggedUserData != null) {
             await _bloc.storeJWTInfo(loggedUserData);
             _bloc.setAuthorizationHeader();
