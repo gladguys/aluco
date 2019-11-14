2582517b9bcf72393fb5ccffc1300ea24ed23ddd
Rodrigo Bastos <rodrigobastosv@gmail.com>
2019-11-13 22:52:58 -0300

    Adjusting the request

--- lib/repository/core/abstract_exam_repository.dart
+++ lib/repository/core/abstract_exam_repository.dart
@@ -2,4 +2,6 @@ import 'package:aluco/model/exam.dart';
 
 import 'abstract_repository.dart';
 
-abstract class AbstractExamRepository extends AbstractRepository<Exam> {}
+abstract class AbstractExamRepository extends AbstractRepository<Exam> {
+  Future<List<Exam>> getByClass(int classId);
+}
