424e2d6195832255f250ca9a0f503a45a719bc16
denis <dineresc@gmail.com>
2019-11-15 15:47:19 -0300

    fix errors build

--- lib/repository/api/exam_repository.dart
+++ lib/repository/api/exam_repository.dart
@@ -1,10 +1,12 @@
 import 'package:aluco/model/exam.dart';
 import 'package:aluco/model/exam_grade_dto.dart';
 import 'package:aluco/model/grades_wrapper.dart';
+import 'package:aluco/repository/api/API.dart';
 
 import 'package:aluco/repository/core/abstract_exam_repository.dart';
 import 'package:aluco/repository/core/base_repository.dart';
 import 'package:aluco/repository/dio/dio_builder.dart';
+import 'package:dio/dio.dart';
 
 class ExamRepository extends BaseRepository<Exam>
     implements AbstractExamRepository {
