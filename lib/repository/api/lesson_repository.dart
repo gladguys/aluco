2bd98a7bdc9ca404b9b21b36341d6c964ebbaad4
Rodrigo Bastos <rodrigobastosv@gmail.com>
2019-11-15 21:44:21 -0300

    backend stuff

---
+++ lib/repository/api/lesson_repository.dart
@@ -0,0 +1,29 @@
+import 'package:aluco/model/lesson_plan.dart';
+import 'package:aluco/repository/core/abstract_lesson_repository.dart';
+import 'package:aluco/repository/core/base_repository.dart';
+import 'package:aluco/repository/dio/dio_builder.dart';
+
+class LessonRepository extends BaseRepository<LessonPlan>
+    implements AbstractLessonPlanRepository {
+  LessonRepository(String basePath, LessonPlan instance)
+      : super(basePath, instance);
+
+  final _dio = DioBuilder.getDio();
+
+  @override
+  Future<List<LessonPlan>> getPlannedLessonsByClass(int classId) async {
+    try {
+      final response = await _dio.get<dynamic>(basePath,
+          queryParameters: <String, dynamic>{'classId': classId.toString()});
+      return List.generate(
+        response.data.length,
+        (int i) {
+          final lessonPlan = LessonPlan();
+          return lessonPlan.fromJson(response.data[i]);
+        },
+      );
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
