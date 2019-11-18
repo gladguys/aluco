9975866d7f9f295ed7b40fd086c6b0c5d39a5cfb
Rodrigo Bastos <rodrigobastosv@gmail.com>
2019-11-18 14:24:25 -0300

    Adding resolver

--- lib/screen/exam/exams_screen.dart
+++ lib/screen/exam/exams_screen.dart
@@ -1,5 +1,4 @@
 import 'package:aluco/model/exam.dart';
-import 'package:aluco/screen/classes/class_home/class_home_bloc.dart';
 import 'package:aluco/widget/al_scaffold.dart';
 import 'package:aluco/widget/al_stream_builder.dart';
 import 'package:aluco/widget/empty_state/exam_empty_state.dart';
@@ -10,24 +9,8 @@ import 'components/exam_list.dart';
 import 'components/save_exam_button.dart';
 import 'exam_bloc.dart';
 
-class ExamsScreen extends StatefulWidget {
-  @override
-  _ExamsScreenState createState() => _ExamsScreenState();
-}
-
-class _ExamsScreenState extends State<ExamsScreen> {
+class ExamsScreen extends StatelessWidget {
   final _bloc = BlocProvider.getBloc<ExamBloc>();
-  final _classBloc = BlocProvider.getBloc<ClassHomeBloc>();
-
-  @override
-  void initState() {
-    getAllExamsByClass();
-    super.initState();
-  }
-
-  Future<void> getAllExamsByClass() async {
-    await _bloc.getExamsByClass(_classBloc.pickedClass.id);
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,7 +18,7 @@ class _ExamsScreenState extends State<ExamsScreen> {
       stream: _bloc.examStream,
       mainWidget: (dynamic exams) => ALScaffold(
         title: 'Minhas Provas',
-        body: _bloc.examsList.isNotEmpty
+        body: exams.isNotEmpty
             ? ExamList(exams)
             : ExamEmptyState(),
         floatingActionButton:
