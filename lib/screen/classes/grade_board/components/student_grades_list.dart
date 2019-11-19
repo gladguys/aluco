16b78c7e34733f2387485066ac92254f11f849de
Alexandre Lima <alexlima.crvg@gmail.com>
2019-11-18 23:24:24 -0300

    Formatting grade board

--- lib/screen/classes/grade_board/components/student_grades_list.dart
+++ lib/screen/classes/grade_board/components/student_grades_list.dart
@@ -1,3 +1,4 @@
+import 'package:aluco/utils/al_number_format.dart';
 import 'package:flutter/material.dart';
 import 'package:aluco/model/exam_grade_dto.dart';
 
@@ -11,10 +12,31 @@ class StudentGradesList extends StatelessWidget {
     return ListView.separated(
       shrinkWrap: true,
       itemBuilder: (_, i) => Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
-          Text(exams[i].examName),
-          const SizedBox(width: 12),
-          Text(exams[i].grade != null ? exams[i].grade.toString() : 'Sem Nota'),
+          Expanded(
+            child: Text(exams[i].examName),
+          ),
+          const SizedBox(width: 8),
+          if (exams[i].grade != null)
+            Text(
+              ALNumberFormat.formatDoubleWithDecimal(
+                number: exams[i].grade.toString(),
+              ),
+              style: TextStyle(
+                fontWeight: FontWeight.w500,
+                color:
+                    exams[i].grade >= 6 ? Colors.green[600] : Colors.red[600],
+              ),
+            )
+          else
+            const Text(
+              'Sem Nota',
+              style: TextStyle(
+                fontSize: 12,
+                color: Colors.grey,
+              ),
+            ),
         ],
       ),
       separatorBuilder: (_, i) => const Divider(),
--- lib/screen/classes/grade_board/grade_board_screen.dart
+++ lib/screen/classes/grade_board/grade_board_screen.dart
@@ -1,8 +1,10 @@
 import 'package:aluco/model/student_grades.dart';
 import 'package:aluco/screen/classes/class_home/class_home_bloc.dart';
+import 'package:aluco/utils/al_number_format.dart';
 import 'package:aluco/widget/al_scaffold.dart';
 import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
+import 'package:gg_flutter_components/loading/gg_loading_double_bounce.dart';
 
 import 'components/student_grades_list.dart';
 import 'grade_board_bloc.dart';
@@ -13,7 +15,6 @@ class GradeBoardScreen extends StatefulWidget {
 }
 
 class _GradeBoardScreenState extends State<GradeBoardScreen> {
-
   Future<List<StudentGrades>> studentsGradeFuture;
   final _bloc = GradeBoardBloc();
 
@@ -33,19 +34,59 @@ class _GradeBoardScreenState extends State<GradeBoardScreen> {
         builder: (_, snapshot) {
           if (snapshot.hasData) {
             final studentsGrades = snapshot.data;
-            return ListView.builder(
+            return ListView.separated(
+              padding: const EdgeInsets.all(8),
+              separatorBuilder: (BuildContext context, int index) {
+                return const SizedBox(height: 6);
+              },
               itemCount: studentsGrades.length,
               itemBuilder: (_, i) {
-                return ExpansionTile(
-                  title: Text('${studentsGrades[i].studentName}: ${studentsGrades[i].average}'),
-                  children: <Widget>[
-                    StudentGradesList(studentsGrades[i].exams),
-                  ],
+                return Material(
+                  elevation: 1,
+                  clipBehavior: Clip.antiAlias,
+                  borderRadius: BorderRadius.circular(8),
+                  child: ExpansionTile(
+                    title: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Expanded(
+                          child: Text('${studentsGrades[i].studentName}'),
+                        ),
+                        const SizedBox(width: 8),
+                        Column(
+                          children: <Widget>[
+                            const Text(
+                              'Média',
+                              style: TextStyle(fontSize: 9),
+                            ),
+                            Text(
+                              ALNumberFormat.formatDoubleWithDecimal(
+                                number: '${studentsGrades[i].average}',
+                              ),
+                              style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                color:
+                                    double.parse(studentsGrades[i].average) >= 6
+                                        ? Colors.green[600]
+                                        : Colors.red[600],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(16, 8, 16, 16),
+                        child: StudentGradesList(studentsGrades[i].exams),
+                      ),
+                    ],
+                  ),
                 );
               },
             );
           }
-          return const CircularProgressIndicator();
+          return const GGLoadingDoubleBounce();
         },
       ),
     );
