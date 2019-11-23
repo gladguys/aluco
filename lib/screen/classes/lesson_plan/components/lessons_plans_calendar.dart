727862f170e36628d4ebc18d48cd463dc820c182
Alexandre Lima <alexlima.crvg@gmail.com>
2019-11-23 17:00:03 -0300

    formatting calendar

--- lib/screen/classes/lesson_plan/components/lessons_plans_calendar.dart
+++ lib/screen/classes/lesson_plan/components/lessons_plans_calendar.dart
@@ -40,41 +40,251 @@ class _LessonsPlansCalendarState extends State<LessonsPlansCalendar> {
         borderRadius: BorderRadius.circular(8),
         side: BorderSide(color: Colors.grey[300]),
       ),
-      child: TableCalendar(
-        calendarController: _calendarController,
-        locale: 'pt_BR',
-        events: _buildLessonsMap(widget.lessonsPlans),
-        availableCalendarFormats: const {
-          CalendarFormat.week: 'Semana',
-          CalendarFormat.month: 'Mês',
-        },
-        headerStyle: HeaderStyle(
-          headerPadding: null,
-          formatButtonTextStyle: TextStyle(color: Colors.grey[600]),
-          formatButtonDecoration: BoxDecoration(
-            border: Border(
-              top: BorderSide(color: Colors.grey),
-              bottom: BorderSide(color: Colors.grey),
-              left: BorderSide(color: Colors.grey),
-              right: BorderSide(color: Colors.grey),
+      child: Padding(
+        padding: const EdgeInsets.only(bottom: 4),
+        child: TableCalendar(
+          calendarController: _calendarController,
+          locale: 'pt_BR',
+          events: _buildLessonsMap(widget.lessonsPlans),
+          availableCalendarFormats: const {
+            CalendarFormat.week: 'Semana',
+            CalendarFormat.month: 'Mês',
+          },
+          headerStyle: HeaderStyle(
+            headerPadding: null,
+            formatButtonTextStyle: TextStyle(color: Colors.grey[600]),
+            formatButtonDecoration: BoxDecoration(
+              border: Border(
+                top: BorderSide(color: Colors.grey),
+                bottom: BorderSide(color: Colors.grey),
+                left: BorderSide(color: Colors.grey),
+                right: BorderSide(color: Colors.grey),
+              ),
+              borderRadius: BorderRadius.circular(8),
             ),
-            borderRadius: BorderRadius.circular(8),
+            titleTextBuilder: (DateTime date, dynamic locale) =>
+                DateFormat.yMMM(locale).format(date),
           ),
-          titleTextBuilder: (DateTime date, dynamic locale) =>
-              DateFormat.yMMM(locale).format(date),
+          builders: CalendarBuilders(
+            dayBuilder: (context, date, list) {
+              return Container(
+                margin: const EdgeInsets.all(4),
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(50),
+                  boxShadow: <BoxShadow>[
+                    BoxShadow(
+                      offset: const Offset(0, 1),
+                      color: Colors.grey[400],
+                    ),
+                    BoxShadow(
+                      offset: const Offset(0, -0.1),
+                      color: Colors.grey[300],
+                    )
+                  ],
+                ),
+                child: Center(
+                  child: Text(
+                    '${date.day}',
+                    style: TextStyle().copyWith(fontSize: 14),
+                  ),
+                ),
+              );
+            },
+            outsideDayBuilder: (context, date, list) {
+              return Container(
+                margin: const EdgeInsets.all(4),
+                width: 100,
+                height: 100,
+                child: Center(
+                  child: Text(
+                    '${date.day}',
+                    style: TextStyle().copyWith(
+                      fontSize: 14,
+                      color: Colors.grey[300],
+                    ),
+                  ),
+                ),
+              );
+            },
+            outsideWeekendDayBuilder: (context, date, list) {
+              return Container(
+                margin: const EdgeInsets.all(4),
+                width: 100,
+                height: 100,
+                child: Center(
+                  child: Text(
+                    '${date.day}',
+                    style: TextStyle().copyWith(
+                      fontSize: 14,
+                      color: Colors.red[100],
+                    ),
+                  ),
+                ),
+              );
+            },
+            todayDayBuilder: (context, date, list) {
+              return Container(
+                margin: const EdgeInsets.all(4),
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(50),
+                  boxShadow: <BoxShadow>[
+                    BoxShadow(
+                      offset: const Offset(0, 1),
+                      color: (date.weekday == 6 || date.weekday == 7)
+                          ? Colors.red[100]
+                          : Colors.grey[400],
+                    ),
+                    BoxShadow(
+                      offset: const Offset(0, -0.1),
+                      color: (date.weekday == 6 || date.weekday == 7)
+                          ? Colors.red[50]
+                          : Colors.grey[300],
+                    )
+                  ],
+                ),
+                child: Center(
+                  child: Text(
+                    '${date.day}',
+                    style: (date.weekday == 6 || date.weekday == 7)
+                        ? TextStyle().copyWith(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.red[300])
+                        : TextStyle().copyWith(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                          ),
+                  ),
+                ),
+              );
+            },
+            weekendDayBuilder: (context, date, list) {
+              return Container(
+                margin: const EdgeInsets.all(4),
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(50),
+                  boxShadow: <BoxShadow>[
+                    BoxShadow(
+                      offset: const Offset(0, 1),
+                      color: Colors.red[100],
+                    ),
+                    BoxShadow(
+                      offset: const Offset(0, -0.1),
+                      color: Colors.red[50],
+                    )
+                  ],
+                ),
+                child: Center(
+                  child: Text(
+                    '${date.day}',
+                    style: TextStyle().copyWith(
+                      fontSize: 14.0,
+                      color: Colors.red[300],
+                    ),
+                  ),
+                ),
+              );
+            },
+            selectedDayBuilder: (context, date, list) {
+              return Container(
+                margin: const EdgeInsets.all(4),
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 2,
+                    color: Colors.orange[600],
+                  ),
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                child: Center(
+                  child: Text(
+                    '${date.day}',
+                    style: (date.weekday == 6 || date.weekday == 7) &&
+                            _calendarController.isToday(date)
+                        ? TextStyle().copyWith(
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.red[300])
+                        : (date.weekday == 6 || date.weekday == 7)
+                            ? TextStyle().copyWith(
+                                fontSize: 14.0, color: Colors.red[300])
+                            : _calendarController.isToday(date)
+                                ? TextStyle().copyWith(
+                                    fontSize: 14.0,
+                                    fontWeight: FontWeight.bold,
+                                  )
+                                : TextStyle().copyWith(fontSize: 14.0),
+                  ),
+                ),
+              );
+            },
+            markersBuilder: (context, date, events, holidays) {
+              final children = <Widget>[];
+
+              if (events.isNotEmpty) {
+                children.add(
+                  Center(
+                    child: _buildEventsMarker(date, events),
+                  ),
+                );
+              }
+
+              return children;
+            },
+          ),
+          onDayLongPressed: (date, _) => _onDayLongPressed(date, _),
+          onDaySelected: (date, _) => _onDaySelected(date, _),
         ),
-        calendarStyle: CalendarStyle(
-          todayColor: theme.primaryColorLight,
-          selectedColor: theme.primaryColor,
-          markersColor: Colors.grey[600],
-          markersAlignment: Alignment.bottomRight,
-          markersPositionRight: 8,
-          outsideStyle: TextStyle(color: Colors.grey[300]),
-          outsideWeekendStyle: TextStyle(color: Colors.red[100]),
-          outsideHolidayStyle: TextStyle(color: Colors.red[100]),
+      ),
+    );
+  }
+
+  Widget _buildEventsMarker(DateTime date, List events) {
+    return Container(
+      margin: const EdgeInsets.all(4.0),
+      width: 40,
+      height: 40,
+      decoration: BoxDecoration(
+        color: theme.primaryColorLight,
+        border: _calendarController.isSelected(date)
+            ? Border.all(width: 2, color: Colors.orange[600])
+            : null,
+        borderRadius: BorderRadius.circular(50),
+        boxShadow: _calendarController.isSelected(date)
+            ? null
+            : <BoxShadow>[
+                BoxShadow(
+                  offset: const Offset(0, 1),
+                  color: Colors.orange[300],
+                ),
+                BoxShadow(
+                  offset: const Offset(0, -0.1),
+                  color: Colors.orange[200],
+                )
+              ],
+      ),
+      child: Center(
+        child: Text(
+          '${date.day}',
+          style: (_calendarController.isToday(date) ||
+                      _calendarController.isSelected(date)) &&
+                  (date.weekday == 6 || date.weekday == 7)
+              ? TextStyle().copyWith(
+                  fontSize: 14.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.red[300])
+              : TextStyle().copyWith(fontSize: 14.0),
         ),
-        onDayLongPressed: (date, _) => _onDayLongPressed(date, _),
-        onDaySelected: (date, _) => _onDaySelected(date, _),
       ),
     );
   }
