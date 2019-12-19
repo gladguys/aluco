e418967ebb35090bbb9f4bcc2461243ee10ecc40
Rodrigo Bastos <rodrigobastosv@gmail.com>
2019-12-19 13:30:07 -0300

    Improve visualization

--- lib/screen/classes/grade_board/components/student_grades_list.dart
+++ lib/screen/classes/grade_board/components/student_grades_list.dart
@@ -1,46 +1,87 @@
+import 'package:aluco/model/exams_period.dart';
 import 'package:aluco/model/period_content.dart';
 import 'package:flutter/material.dart';
+import 'package:toggle_switch/toggle_switch.dart';
 
-class StudentGradesList extends StatelessWidget {
+class StudentGradesList extends StatefulWidget {
   const StudentGradesList(this.allPeriodsContent);
 
   final List<PeriodContent> allPeriodsContent;
 
   @override
+  _StudentGradesListState createState() => _StudentGradesListState();
+}
+
+class _StudentGradesListState extends State<StudentGradesList> {
+  int index;
+  List<PeriodContent> get allPeriodsContent => widget.allPeriodsContent;
+
+  @override
+  void initState() {
+    index = 0;
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      shrinkWrap: true,
-      itemBuilder: (_, i) => Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          Expanded(
-            //child: Text(exams[i].examName),
-            child: Text(''),
-          ),
-          const SizedBox(width: 8),
-          /*if (exams[i].grade != null)
-            Text(
-              ALNumberFormat.formatDoubleWithDecimal(
-                number: exams[i].grade.toString(),
-              ),
-              style: TextStyle(
-                fontWeight: FontWeight.w500,
-                color:
-                    exams[i].grade >= 6 ? Colors.green[600] : Colors.red[600],
-              ),
-            )
-          else
-            const Text(
-              'Sem Nota',
-              style: TextStyle(
-                fontSize: 12,
-                color: Colors.grey,
-              ),
-            ),*/
-        ],
-      ),
-      separatorBuilder: (_, i) => const Divider(),
-      itemCount: 0,
+    return Column(
+      children: <Widget>[
+        ToggleSwitch(
+          minWidth: 90.0,
+          initialLabelIndex: 0,
+          activeBgColor: Colors.redAccent,
+          activeTextColor: Colors.white,
+          inactiveBgColor: Colors.grey,
+          inactiveTextColor: Colors.grey[900],
+          labels: const [
+            'Bimestre 1',
+            'Bimestre 2',
+            'Bimestre 3',
+            'Bimestre 4'
+          ],
+          onToggle: (pickedIndex) {
+            setState(() {
+              index = pickedIndex;
+            });
+          },
+        ),
+        const SizedBox(height: 18),
+        _buildPeriodContent(allPeriodsContent[index].examsPeriod ?? [],
+            allPeriodsContent[index].average),
+      ],
     );
+  }
+
+  Widget _buildPeriodContent(
+      List<ExamsPeriod> periodExams, double periodAverage) {
+    if (periodExams.isNotEmpty) {
+      return ListView.builder(
+        shrinkWrap: true,
+        itemBuilder: (_, i) {
+          if (i < allPeriodsContent[index].examsPeriod.length) {
+            return ListTile(
+              title: Text(allPeriodsContent[index].examsPeriod[i].examName),
+              trailing: Text(
+                  allPeriodsContent[index].examsPeriod[i].grade?.toString() ??
+                      'Sem Nota'),
+            );
+          } else {
+            return ListTile(
+              title: const Text('Média do Bimestre'),
+              trailing: Text(
+                  allPeriodsContent[index].average?.toString() ?? 'Sem Média'),
+            );
+          }
+        },
+        itemCount: (allPeriodsContent[index].examsPeriod?.isEmpty ?? true)
+            ? 0
+            : allPeriodsContent[index].examsPeriod.length + 1,
+      );
+    } else {
+      return const Text(
+        'Não existem provas para este bimestre',
+        style: TextStyle(color: Colors.red),
+      );
+    }
   }
 }
