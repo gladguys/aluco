a4ee1c07e2ec7b77422d7b910085f26d99a4ce85
Rodrigo Bastos <rodrigobastosv@gmail.com>
2020-01-11 23:04:13 -0300

    Addng walktrough

--- lib/screen/intro/into_screen.dart
+++ lib/screen/intro/into_screen.dart
@@ -1,15 +1,11 @@
 import 'package:aluco/core/routing/al_router.dart';
 import 'package:aluco/screen/home/home_screen.dart';
-import 'package:aluco/screen/signin/signin_bloc.dart';
-import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
 import 'package:introduction_screen/introduction_screen.dart';
 
 import 'intro_pages.dart';
 
 class IntroScreen extends StatelessWidget {
-  final SigninBloc _bloc = BlocProvider.getBloc<SigninBloc>();
-
   @override
   Widget build(BuildContext context) {
     return IntroductionScreen(
@@ -18,7 +14,6 @@ class IntroScreen extends StatelessWidget {
       skip: const Text('Skip'),
       done: const Text('Done', style: TextStyle(fontWeight: FontWeight.w600)),
       onDone: () async {
-        await _bloc.markFirstLoginDone();
         ALRouter.pushAndReplace(context, HomeScreen());
       },
       dotsDecorator: DotsDecorator(
